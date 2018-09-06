511995f1fe61e8180d4889495a54ee6461519f34
Pieter Hordijk <info@pieterhordijk.com>
2018-09-06 21:22:07 +0300

    Added missing tests

---
+++ tests/Fakes/Service/VirusTotal/InvalidUploadResponse.php
@@ -0,0 +1,88 @@
+<?php declare(strict_types=1);
+
+namespace PeeHaa\AsyncVirusScanTest\Fakes\Service\VirusTotal;
+
+use Amp\Artax\ConnectionInfo;
+use Amp\Artax\MetaInfo;
+use Amp\Artax\Request;
+use Amp\Artax\Response;
+use Amp\ByteStream\InputStream;
+use Amp\ByteStream\Message;
+use Amp\Success;
+use PHPUnit\Framework\MockObject\Generator;
+use PHPUnit\Framework\MockObject\MockObject;
+
+class InvalidUploadResponse implements Response
+{
+    public function getProtocolVersion(): string
+    {
+        return '1.1';
+    }
+
+    public function getStatus(): int
+    {
+        return 200;
+    }
+
+    public function getReason(): string
+    {
+        return '';
+    }
+
+    public function getRequest(): Request
+    {
+        return new Request('https://example.com');
+    }
+
+    public function getOriginalRequest(): Request
+    {
+        return new Request('https://example.com');
+    }
+
+    public function getPreviousResponse()
+    {
+        return null;
+    }
+
+    // phpcs:disable SlevomatCodingStandard.Functions.UnusedParameter.UnusedParameter
+    public function hasHeader(string $field): bool
+    {
+        return false;
+    }
+
+    public function getHeader(string $field)
+    {
+        return null;
+    }
+
+    public function getHeaderArray(string $field): array
+    {
+        return [];
+    }
+    // phpcs:enable
+
+    public function getHeaders(): array
+    {
+        return [];
+    }
+
+    public function getBody(): Message
+    {
+        /** @var MockObject|InputStream $inputStream */
+        $inputStream = (new Generator())->getMock(InputStream::class);
+
+        $inputStream
+            ->method('read')
+            ->willReturnOnConsecutiveCalls(new Success(json_encode([
+                'response_code' => 0,
+            ])), new Success(null))
+        ;
+
+        return new Message($inputStream);
+    }
+
+    public function getMetaInfo(): MetaInfo
+    {
+        return new MetaInfo(new ConnectionInfo('127.0.0.1', '8.8.8.8'));
+    }
+}
--- tests/Unit/Service/VirusTotal/ServiceTest.php
+++ tests/Unit/Service/VirusTotal/ServiceTest.php
@@ -4,20 +4,27 @@ namespace PeeHaa\AsyncVirusScanTest\Unit\VirusTotal;
 
 use Amp\Artax\Request;
 use Amp\Loop;
+use Amp\Promise;
 use Amp\Success;
 use PeeHaa\AsyncVirusScan\Http\Client;
 use PeeHaa\AsyncVirusScan\Service\BadRequest;
+use PeeHaa\AsyncVirusScan\Service\Exception;
 use PeeHaa\AsyncVirusScan\Service\FileTooLarge;
 use PeeHaa\AsyncVirusScan\Service\Forbidden;
 use PeeHaa\AsyncVirusScan\Service\RateLimit;
+use PeeHaa\AsyncVirusScan\Service\TimeOut;
 use PeeHaa\AsyncVirusScan\Service\VirusTotal\ApiKey;
 use PeeHaa\AsyncVirusScan\Service\VirusTotal\Service;
 use PeeHaa\AsyncVirusScanTest\Fakes\FileSystem\LargeFile;
 use PeeHaa\AsyncVirusScanTest\Fakes\FileSystem\ValidFile;
 use PeeHaa\AsyncVirusScanTest\Fakes\Service\VirusTotal\BadRequestResponse;
 use PeeHaa\AsyncVirusScanTest\Fakes\Service\VirusTotal\ForbiddenResponse;
+use PeeHaa\AsyncVirusScanTest\Fakes\Service\VirusTotal\InvalidUploadResponse;
 use PeeHaa\AsyncVirusScanTest\Fakes\Service\VirusTotal\RateLimitResponse;
+use PeeHaa\AsyncVirusScanTest\Fakes\Service\VirusTotal\TimeOutResponse;
 use PeeHaa\AsyncVirusScanTest\Fakes\Service\VirusTotal\ValidResponse;
+use PeeHaa\AsyncVirusScanTest\Fakes\Service\VirusTotal\ValidUploadResponseWithoutPositives;
+use PeeHaa\AsyncVirusScanTest\Fakes\Service\VirusTotal\ValidUploadResponseWithPositives;
 use PHPUnit\Framework\MockObject\MockObject;
 use PHPUnit\Framework\TestCase;
 
@@ -124,5 +131,103 @@ class ServiceTest extends TestCase
 
             yield $service->scan(new ValidFile());
         });
+    }
+
+    public function testScanThrowsOnInvalidUploadRequest()
+    {
+        $this->expectException(Exception::class);
+        $this->expectExceptionMessage('File not correctly uploaded to the service.');
+
+        $this->httpClient
+            ->expects($this->at(0))
+            ->method('request')
+            ->willReturn(new Success(new ValidResponse()))
+        ;
+
+        $this->httpClient
+            ->expects($this->at(1))
+            ->method('request')
+            ->willReturn(new Success(new InvalidUploadResponse()))
+        ;
+
+        Loop::run(function() {
+            $service = new Service($this->apiKey, $this->httpClient);
+
+            yield $service->scan(new ValidFile());
+        });
+    }
+
+    public function testScanReturnsTrueWhenPositivesAreFound()
+    {
+        $this->httpClient
+            ->expects($this->at(0))
+            ->method('request')
+            ->willReturn(new Success(new ValidResponse()))
+        ;
+
+        $this->httpClient
+            ->expects($this->at(1))
+            ->method('request')
+            ->willReturn(new Success(new ValidUploadResponseWithPositives()))
+        ;
+
+        Loop::run(function() {
+            $service = new Service($this->apiKey, $this->httpClient);
+
+            $this->assertTrue(yield $service->scan(new ValidFile()));
+        });
+    }
+
+    public function testScanReturnsFalseWhenNoPositivesAreFound()
+    {
+        $this->httpClient
+            ->expects($this->at(0))
+            ->method('request')
+            ->willReturn(new Success(new ValidResponse()))
+        ;
+
+        $this->httpClient
+            ->expects($this->at(1))
+            ->method('request')
+            ->willReturn(new Success(new ValidUploadResponseWithoutPositives()))
+        ;
+
+        Loop::run(function() {
+            $service = new Service($this->apiKey, $this->httpClient);
+
+            $this->assertFalse(yield $service->scan(new ValidFile()));
+        });
+    }
+
+    public function testScanTimesOutAfterTenMinutes()
+    {
+        $this->markTestSkipped('Fix this test please.');
+
+        $this->expectException(TimeOut::class);
+        $this->expectExceptionMessage('Service timed out.');
+
+        $this->httpClient
+            ->method('request')
+            ->willReturnCallback([$this, 'getTimeOutFlowResponses'])
+        ;
+
+        Loop::run(function() {
+            $service = new Service($this->apiKey, $this->httpClient);
+
+            yield $service->scan(new ValidFile());
+        });
+    }
+
+    public function getTimeOutFlowResponses(): Promise
+    {
+        static $mustRespondWithTimeOut = false;
+
+        if ($mustRespondWithTimeOut === true) {
+            return new Success(new TimeOutResponse());
+        }
+
+        $mustRespondWithTimeOut = true;
+
+        return new Success(new ValidResponse());
     }
 }
