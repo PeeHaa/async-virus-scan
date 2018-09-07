103584242acf8fd4655a6ce6a1be8c7d682910dc
Pieter Hordijk <info@pieterhordijk.com>
2018-09-07 17:06:17 +0300

    Start of integration tests

---
+++ tests/Integration/Service/VirusTotal/CleanFileTest.php
@@ -0,0 +1,13 @@
+<?php declare(strict_types=1);
+
+namespace PeeHaa\AsyncVirusScanTest\Integration\Service\VirusTotal;
+
+use PHPUnit\Framework\TestCase;
+
+class CleanFileTest extends TestCase
+{
+    public function testReturnsTrue()
+    {
+        $this->assertTrue(true);
+    }
+}
